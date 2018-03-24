119b2939caad3a408609e02657de6a85156557b6
Corned <oliver@kaarnalehto.fi>
2018-03-24 18:37:53 +0200

    header

--- src/BrowserApp.js
+++ src/BrowserApp.js
@@ -1,11 +1,15 @@
+// npm modules
 import React from "react"
 import { connect } from "react-redux"
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
+// components
 import Communities from "./components/Communities"
 import CommunityNavigation from "./components/CommunityNavigation"
 import Post from "./components/Post"
 import LoginForm from "./components/LoginForm"
 
+// reducers
 import { communityInit } from "./reducers/community"
 import { postInit } from "./reducers/post"
 import { login, logout } from "./reducers/loggedUser"
@@ -35,37 +39,44 @@ class App extends React.Component {
 
 	render() {
 		return (
-			<div>
-				<CommunityNavigation/>	
-				<div id="content">
-					<Communities/>
-					<div className="drop-shadow" id="posts">
-						{this.props.posts.map((post) => <Post key={post.id} post={post}/>)}
+			<Router>
+				<div>
+					<div id="header">
+						<img src="https://i.imgur.com/SXuHOGK.png"/>
 					</div>
 
-					{this.isLoggedIn() ?
-						<div id="sidebar">
-							<div id="user-information" className="frame drop-shadow">
-								<p className="sidebar-title">{this.props.loggedUser.user.username}</p>
-								<form onSubmit={this.logout}>
-									<button type="submit">Logout</button>
-								</form>
-							</div>
-							<div id="submit-selector" className="frame drop-shadow">
-								<button id="submit-link">Submit Link</button>
-								<button id="submit-text">Submit Text</button>
-								<button id="create-community">Create a Community</button>
-							</div>
+					<Route path="(/|/c)" render={() => <CommunityNavigation/>}/>
+
+					<div id="content">
+						<Communities/>
+						<div className="drop-shadow" id="posts">
+							{this.props.posts.map((post) => <Post key={post.id} post={post}/>)}
 						</div>
-						:
-						<div id="sidebar">
-							<div className="frame drop-shadow">
-								<LoginForm/>
+
+						{this.isLoggedIn() ?
+							<div id="sidebar">
+								<div id="user-information" className="frame drop-shadow">
+									<p className="sidebar-title">{this.props.loggedUser.user.username}</p>
+									<form onSubmit={this.logout}>
+										<button type="submit">Logout</button>
+									</form>
+								</div>
+								<div id="submit-selector" className="frame drop-shadow">
+									<button id="submit-link">Submit Link</button>
+									<button id="submit-text">Submit Text</button>
+									<button id="create-community">Create a Community</button>
+								</div>
 							</div>
-						</div>
-					}
+							:
+							<div id="sidebar">
+								<div className="frame drop-shadow">
+									<LoginForm/>
+								</div>
+							</div>
+						}
+					</div>
 				</div>
-			</div>
+			</Router>
 		)
 	}
 }
