6ada86533917c22be615644254236c1e369258e5
Corned <oliver@kaarnalehto.fi>
2018-04-05 21:40:27 +0300

    yksittäisen textpostin sivu näyttää tiedot oikein

--- src/components/community/ExpandedPost.js
+++ src/components/community/ExpandedPost.js
@@ -39,20 +39,20 @@ class ExpandedPost extends React.Component {
 			postAdded = `${deltaTimeDays === 1 ? "a" : deltaTimeDays} day${(deltaTimeDays === 1 ? "" : "s")} ago`
 		}
 
-		console.log(post.body)
-
 		return (
-			<div className="frame">
-				<div className="thumbnail">
-					<img alt="post-thumbnail" src="https://i.imgur.com/a7TZ0Yo.png"/>
-				</div>
-				<div className="post-info">
-					<p className="post-title">{post.title}<span className="small-text light-text">(imgur.com)</span></p>
-					<p className="post-added"><span className="light-text">submitted</span> {postAdded} <span className="light-text">by</span> <span className="poster">{post.user.username}</span></p>
-					<p className="post-other"><span className="comments">0 Comments - c/{post.community.name}</span></p>
+			<div className="single-post frame">
+				<div className="post" style={{ marginBottom: "1em" }}>
+					<div className="thumbnail">
+						<img alt="post-thumbnail" src="https://i.imgur.com/a7TZ0Yo.png"/>
+					</div>
+					<div className="post-info">
+						<p className="post-title">{post.title}<span className="small-text light-text">(imgur.com)</span></p>
+						<p className="post-added"><span className="light-text">submitted</span> {postAdded} <span className="light-text">by</span> <span className="poster">{post.user.username}</span></p>
+						<p className="post-other"><span className="comments">0 Comments - c/{post.community.name}</span></p>
+					</div>
 				</div>
 
-				<p>{this.props.post.body}</p>
+				<p>{post.body}</p>
 			</div>
 		)
 	}
