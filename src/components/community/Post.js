1c964b05bf403f99cf6f54045c05046daf20c3ed
Corned <oliver@kaarnalehto.fi>
2018-04-09 15:45:55 +0300

    Post komponettia refaktoroitu, käytetään Link komponenttia navigointiin

--- src/components/community/Post.js
+++ src/components/community/Post.js
@@ -1,28 +1,25 @@
 import React from "react"
 import { connect } from "react-redux"
-import { withRouter } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 import { getPostById } from "../../reducers/postReducer"
 
 import TimeSince from "../../utils/TimeSince"
 
-const Post = (props) => {
-	const post = props.post
-
-	const toPost = () => {
-		props.history.push(`/c/${post.community.name}/post/${post.id}`)
-		props.getPostById(post.id)
-	}
-	
+const Post = ({ post }) => {
 	post.date = TimeSince(post.date)
+
+	const location = `/c/${post.community.name}/post/${post.id}`
 
 	return (
 		<div className="post frame">
 			<div className="post__thumbnail">
-				<img alt="postthumbnail" src="https://i.imgur.com/a7TZ0Yo.png"/>
+				<img alt="postthumbnail" src="https://i.imgur.com/lFII83x.png"/>
 			</div>
 			<div className="post__info">
-				<p onClick={toPost} className="post__title clickable">{post.title}</p>
+				<Link to={location}>
+					<p className="post__title clickable">{post.title}</p>
+				</Link>
 			</div>
 		</div>
 	)
@@ -30,4 +27,4 @@ const Post = (props) => {
 
 const mapDispatchToProps = { getPostById }
 
-export default connect(null, mapDispatchToProps)(withRouter(Post))
+export default connect(null, mapDispatchToProps)(Post)
