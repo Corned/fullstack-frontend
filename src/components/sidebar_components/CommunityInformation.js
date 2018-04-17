c445fa62b215b5e7efedb400909f28352b2ddfe5
Corned <oliver@kaarnalehto.fi>
2018-04-17 16:30:49 +0300

    Subscribe nappi

--- src/components/sidebar_components/CommunityInformation.js
+++ src/components/sidebar_components/CommunityInformation.js
@@ -7,7 +7,7 @@ const CommunityInformation = ({ community, setView }) => {
 	return (
 		<div id="community-information" className="frame apply-margin--vertical">
 			<h1>{community.name}</h1>
-			<p className="secondary-text">{community.members.length} members</p>
+			<p className="secondary-text">{community.members.length} member{community.members.length === 1 ? "" : "s"}</p>
 
 			<Link to={`${submitButtonUrl}submit-text`} onClick={setView("submit-text")}>
 				<button style={{width: "100%"}}>Submit Your Thoughts</button>
@@ -16,6 +16,8 @@ const CommunityInformation = ({ community, setView }) => {
 			<Link to={`${submitButtonUrl}submit-link`} onClick={setView("submit-link")}>
 				<button style={{width: "100%"}}>Share a Link</button>
 			</Link>
+
+			<button className="disabled" style={{width: "100%"}}>(Un)Subscribe!</button>
 		</div>
 	)
 }
